5000a86bed64dd36e09222b68cbfdaaca828b92c
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2026-03-19 12:41:22 +0700

    Update dependencies

--- front-end/nuxt.config.ts
+++ front-end/nuxt.config.ts
@@ -1,15 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2025-05-15",
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  modules: ["@unocss/nuxt", "@nuxt/a11y", "@nuxt/hints", "@nuxt/test-utils"],
   nitro: {
     preset: "bun",
   },
-  modules: [
-    "@nuxt/fonts",
-    "@nuxt/image",
-    "@nuxt/scripts",
-    "@nuxt/test-utils",
-    "@unocss/nuxt",
-  ],
 });
